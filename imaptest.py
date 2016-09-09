a2f19641f339bb6fc2193d55b621a0470f4053c8
Mariusz Francka <mariusz.francka@corp.ovh.com>
2016-09-09 12:37:40 +0200

    first patches

--- imaptest.py
+++ imaptest.py
@@ -25,77 +25,137 @@ import imaplib, getopt
 import random, time, threading
 import csv, sys, signal
 
+
+class timer:
+    def __init__(self):
+        self.start_time = 0
+
+    def start(self):
+        self.start_time = time.perf_counter()
+
+    def end(self):
+        self.counted_time = time.perf_counter() - self.start_time
+        return self.counted_time
+
+
 class imap_test(threading.Thread):
         def __init__(self, login, passwd, server):
                 threading.Thread.__init__(self)
                 self.login = login
                 self.passwd = passwd
                 self.server = server
+
         def run(self):
                 global running
+                counter = timer()
                 lock = threading.Lock()
                 try:
                         M = imaplib.IMAP4_SSL(self.server)
                         self.sleep()
+                        counter.start()
                         M.login(self.login, self.passwd)
-                        print 'logged in:\t {}'.format(self.login)
-                except:
                         lock.acquire()
-                        print 'Login failed for {}.'.format(self.login)
+                        counter.end()
+                        lock.release()
+                        print('logged in:\t {} in {}s'.
+                              format(self.login, counter.counted_time))
+                except Exception as e:
+                        lock.acquire()
+                        print('Login failed for {}.'.format(self.login))
+                        print(e)
                         lock.release()
                         sys.exit(1)
                 else:
                         M.select()
                         while running:
-                                typ, data = M.list()
-                                self.sleep()
+                            counter.start()
+                            typ, data = M.list()
+                            print(counter.end())
+                            self.sleep()
                         lock.acquire()
-                        print 'Stopping:\t {}'.format(self.login)
+                        print('Stopping:\t {}'.format(self.login))
                         lock.release()
-         #               M.close()
                         M.logout()
-        def sleep(self):
-                time.sleep(random.randint(1,3))
 
-def read_logins(loginfile):
+        def sleep(self):
+                time.sleep(random.randint(1, 3))
+
+
+def read_logins(loginfile, master_password=None, master_server=None):
         logins = []
         try:
                 with open(loginfile, 'r') as f:
                         reader = csv.reader(f, delimiter=':')
                         for row in reader:
-                                logins.append(row)
+                            if master_password:
+                                if len(row) >= 2:
+                                    row[1] = master_password
+                                else:
+                                    row.append(master_password)
+                            if master_server:
+                                if len(row) >= 3:
+                                    row[2] = master_server
+                                else:
+                                    row.append(master_server)
+                            logins.append(row)
                 return logins
         except:
-                print 'Error reading {}'.format(loginfile)
+                print('Error reading {}'.format(loginfile))
                 raise
                 sys.exit()
 
+
 def sighandler(num, stack):
-        print 'Ctrl+C pressed...'
+        print('Ctrl+C pressed...')
         global running
         running = False
 
+
+def help():
+    print('imaptest.py -l <login file> [-p <master_password>] '
+          '[-s <master_server>] [-n <number_of_users_from_file>]')
+
+
 def main(argv):
         loginfile = 'logins.txt'
+        master_password = None
+        master_server = None
+        max_users = 0
         threads = []
+        if not argv:
+            help()
+            sys.exit()
         try:
-                opts, args = getopt.getopt(argv, '-l:')
+            opts, args = getopt.getopt(argv, 'l:p:s:n:')
         except getopt.GetoptError:
-                print 'imaptest.py -l <login file>'
+                help()
                 sys.exit()
         for opt, arg in opts:
                 if opt == '-l':
                         loginfile = arg
-        logins = read_logins(loginfile)
-        for credential in logins:
+                if opt == '-p':
+                    print('Using master password for logins.')
+                    master_password = arg
+                if opt == '-s':
+                    master_server = arg
+                    print('Using master server for logins: {}'
+                          .format(master_server))
+                if opt == '-n':
+                    max_users = int(arg)
+                    print('Use {} users'.format(max_users))
+        logins = read_logins(loginfile, master_password, master_server)
+        if max_users == 0:
+            max_users = len(logins)
+        for credential in logins[:max_users]:
                 try:
-                        threadx = imap_test(credential[0], credential[1], credential[2])
+                        threadx = imap_test(credential[0], credential[1],
+                                            credential[2])
                         threadx.start()
                         threads.append(threadx)
-                except Exception, errtxt:
-                        print errtxt
+                except Exception as errtxt:
+                        print(errtxt)
         while True:
-                if not any([thread.isAlive() for thread in threads]):              
+                if not any([thread.isAlive() for thread in threads]):
                         break
                 else:
                         time.sleep(1)
